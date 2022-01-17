edaeb0303bc06bd9446a9068daa5af99536bce96
mindyleblancC <70272873+mindyleblancC@users.noreply.github.com>
2022-01-17 13:36:35 +0200

    Created Generic Grid table according to given config

--- src/Grid.js
+++ src/Grid.js
@@ -1,22 +1,40 @@
 import React from 'react';
 
+const getTHead = (data, config) => {
+  let field;
+  let title;
+  return (
+    <thead>
+      <tr>
+        {Object.keys(data[0]).map((val, k) => (
+          field = config.find(config => config['field'] === val),
+          title = field && field.title,
+          <th key={k} feild={val} title={title}>{val}</th>))}
+      </tr>
+    </thead>
+  );
+}
+
+const getTRow = (data, config) => {
+  let value;
+  let field;
+  return (
+    data.map((obj) => (
+      < tr >
+        {Object.keys(obj).map((key, k) => (
+          field = config.find(config => config['field'] === key),
+          value = (field && field.component ? <field.component data={obj[key]}/> : obj[key]),
+          <td key={k}>{value}</td>
+        ))}
+      </tr >
+    )));
+}
+
 const Grid = ({ config, data }) => (
   <table>
-    <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
-    </thead>
+    {getTHead(data, config)}
     <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
+      {getTRow(data, config)}
     </tbody>
   </table>
 );
